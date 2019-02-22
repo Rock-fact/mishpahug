8f288839806d6cea95793b4c743df5bd92d8a239
Vlada <vlgor@list.ru>
2019-02-22 15:34:20 +0200

    event related dto changed

--- app/src/main/java/com/kor/foodmanager/data/model/EventDto.java
+++ app/src/main/java/com/kor/foodmanager/data/model/EventDto.java
@@ -1,5 +1,7 @@
 package com.kor.foodmanager.data.model;
 
+import android.location.Address;
+
 import java.sql.Time;
 import java.util.Date;
 import java.util.List;
@@ -13,8 +15,113 @@ public class EventDto {
     private List<UserDto> participants;
     private List<String> food;
     private UserDto owner;
+    private EventAddress address;
 
     public EventDto() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getHoliday() {
+        return holiday;
+    }
+
+    public void setHoliday(String holiday) {
+        this.holiday = holiday;
+    }
+
+    public String getConfession() {
+        return confession;
+    }
+
+    public void setConfession(String confession) {
+        this.confession = confession;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public List<UserDto> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<UserDto> participants) {
+        this.participants = participants;
+    }
+
+    public List<String> getFood() {
+        return food;
+    }
+
+    public void setFood(List<String> food) {
+        this.food = food;
+    }
+
+    public UserDto getOwner() {
+        return owner;
+    }
+
+    public void setOwner(UserDto owner) {
+        this.owner = owner;
+    }
+
+    public EventAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(EventAddress address) {
+        this.address = address;
     }
 }
 
