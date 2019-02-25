69daabfe1874b8f1a24537d61813f3ac5c631fea
Vlada <vlgor@list.ru>
2019-02-25 15:42:37 +0200

    MyEvent xmls added

--- app/src/main/java/com/kor/foodmanager/ui/eventInfo/MyEventInfoInProgressFragment.java
+++ app/src/main/java/com/kor/foodmanager/ui/eventInfo/MyEventInfoInProgressFragment.java
@@ -12,10 +12,10 @@ import com.kor.foodmanager.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EventInfoFragment extends Fragment {
+public class MyEventInfoInProgressFragment extends Fragment {
 
 
-    public EventInfoFragment() {
+    public MyEventInfoInProgressFragment() {
         // Required empty public constructor
     }
 
@@ -24,7 +24,7 @@ public class EventInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_info, container, false);
+        return inflater.inflate(R.layout.fragment_my_event_info_in_progress, container, false);
     }
 
 }
