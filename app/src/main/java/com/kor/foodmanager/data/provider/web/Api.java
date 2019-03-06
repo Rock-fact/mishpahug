2b7d781ae6ca1a8f5204879a71ed524956fc3cc1
vlada <vlgor@list.ru>
2019-03-06 09:53:11 +0200

    api loggined list request

--- app/src/main/java/com/kor/foodmanager/data/provider/web/Api.java
+++ app/src/main/java/com/kor/foodmanager/data/provider/web/Api.java
@@ -30,6 +30,9 @@ public interface Api {
     @POST("/event/allprogresslist")
     Call<EventListDto> getListOfEventsInProgress(@Query("page") int page, @Query("size") int size);
 
+    @POST("/event/allprogresslist")
+    Call<EventListDto> getLoginedListOfEventsInProgress(@Header("Authorization") String token, @Query("page") int page, @Query("size") int size);
+
 
     //Authorized requests
     //---------------------------------------------------------------------------------------------
