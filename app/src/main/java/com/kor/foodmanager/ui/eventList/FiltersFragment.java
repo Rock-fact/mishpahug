2731b4933799447e250b13a455d3002123d56a4a
vlada <vlgor@list.ru>
2019-03-13 10:34:24 +0200

    Filters in progress

---
+++ app/src/main/java/com/kor/foodmanager/ui/eventList/FiltersFragment.java
@@ -0,0 +1,44 @@
+package com.kor.foodmanager.ui.eventList;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import com.kor.foodmanager.R;
+import com.kor.foodmanager.data.model.StaticfieldsDto;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+public class FiltersFragment extends Fragment {
+private Unbinder unbinder;
+private StaticfieldsDto staticfields;
+
+@BindView(R.id.confession_spinner)
+    Spinner confessionSpinner;
+
+    public FiltersFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_filters, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
+    }
+}
