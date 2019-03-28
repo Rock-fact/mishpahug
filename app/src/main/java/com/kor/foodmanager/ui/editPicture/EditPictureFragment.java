78ef2c12fbe7e4906d195411d07a377a3e9f3d8b
vlada <vlgor@list.ru>
2019-03-28 14:21:09 +0200

    tried to add crop

--- app/src/main/java/com/kor/foodmanager/ui/editPicture/EditPictureFragment.java
+++ app/src/main/java/com/kor/foodmanager/ui/editPicture/EditPictureFragment.java
@@ -35,6 +35,7 @@ public class EditPictureFragment extends Fragment implements View.OnClickListene
 
     private static final int AVATAR_EDIT_REQUEST = 1;
     private static final int EVENT_BANNER_EDIT_REQUEST = 2;
+    private static final int CROP_REQUEST = 3;
 
     @BindView(R.id.avatar_img)
     ImageView avatar;
@@ -55,9 +56,8 @@ public class EditPictureFragment extends Fragment implements View.OnClickListene
         Picasso.get().load("http://i.imgur.com/DvpvklR.png")
                 .transform(new CropCircleTransformation())
                 .into(avatar);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(eventBanner);
-//        Picasso.get().load("http://i.imgur.com/DvpvklR.png").fit()
-//                .into(eventBanner);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").fit().into(eventBanner);
+
         avatar.setOnClickListener(this);
         eventBanner.setOnClickListener(this);
         return view;
@@ -96,9 +96,30 @@ public class EditPictureFragment extends Fragment implements View.OnClickListene
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK & requestCode == AVATAR_EDIT_REQUEST ||
+                requestCode == EVENT_BANNER_EDIT_REQUEST) {
             Uri picUri = data.getData();
             if (picUri != null) {
+//                Intent intent = new Intent("com.android.camera.action.CROP");
+//                intent.setType("image/*");
+//                intent.setData(picUri);
+//                intent.putExtra("outputX", 200);
+//                intent.putExtra("outputY", 200);
+//                intent.putExtra("aspectX", 1);
+//                intent.putExtra("aspectY", 1);
+//                intent.putExtra("scale", true);
+//                intent.putExtra("uri", picUri);
+
+//                switch (requestCode) {
+//                    case AVATAR_EDIT_REQUEST:
+//                        intent.putExtra("position", AVATAR_EDIT_REQUEST);
+//                        break;
+//                    case EVENT_BANNER_EDIT_REQUEST:
+//                        intent.putExtra("position", EVENT_BANNER_EDIT_REQUEST);
+//                        break;
+//                }
+//
+//                startActivityForResult(intent, CROP_REQUEST);
                 RequestCreator rc = Picasso.get().load(picUri);
                 rc.fetch();
                 switch (requestCode) {
@@ -106,11 +127,24 @@ public class EditPictureFragment extends Fragment implements View.OnClickListene
                         rc.transform(new CropCircleTransformation()).into(avatar);
                         break;
                     case EVENT_BANNER_EDIT_REQUEST:
-                        rc.into(eventBanner); //TODO fit()
+                        rc.fit().into(eventBanner); //TODO fit()
                         break;
                 }
 
             }
+//        } else if (resultCode == RESULT_OK & requestCode == CROP_REQUEST) {
+//            data.getIntExtra("position", 0);
+//            RequestCreator rc = Picasso.get().load(data.getData());
+//            switch (requestCode) {
+//                case AVATAR_EDIT_REQUEST:
+//                    rc.transform(new CropCircleTransformation()).into(avatar);
+//                    break;
+//                case EVENT_BANNER_EDIT_REQUEST:
+//                    rc.fit().into(eventBanner); //TODO fit()
+//                    break;
+//            }
         }
     }
+
+
 }
